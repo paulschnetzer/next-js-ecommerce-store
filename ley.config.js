6ce7b8560764c7c21f6ecd0b7b65e2b63a27e232
Paul Schnetzer <1998sheldon@gmail.com>
2020-10-21 15:48:31 +0200

    cc:wq

--- ley.config.js
+++ ley.config.js
@@ -1,4 +1,4 @@
-const setPostgresDefaultsOnHeroku = require('./util/extractHerokuDatabaseEnvVars');
+const setPostgresDefaultsOnHeroku = require('./util/setPostgresDefaultsOnHeroku');
 
 setPostgresDefaultsOnHeroku();
 
